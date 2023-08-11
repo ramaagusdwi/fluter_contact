ada46614fa7ce60781def41f6b5c1ea8fd8e3bab
Rama Agus <rama.agus@majoo.id>
2023-08-11 19:54:38 +0700

    add contact repository

---
+++ packages/contacts_repository/lib/contacts_repository.dart
@@ -0,0 +1,4 @@
+library contacts_repository;
+
+export 'package:contact_api/contact_api.dart' show ContactModel;
+export 'src/contacts_repository.dart';
---
+++ packages/contacts_repository/lib/src/contacts_repository.dart
@@ -0,0 +1,33 @@
+import 'package:contact_api/contact_api.dart';
+
+/// {@template ContactModels_repository}
+/// A repository that handles `ContactModel` related requests.
+/// {@endtemplate}
+class ContactRepository {
+  /// {@macro ContactModels_repository}
+  const ContactRepository({
+    required ContactsApi contactApi,
+  }) : _contactsApi = contactApi;
+
+  final ContactsApi _contactsApi;
+
+  /// Provides a [Stream] of all ContactModels.
+  Stream<List<ContactModel>> getContacts() => _contactsApi.getContacts();
+
+  /// Saves a [Contact].
+  ///
+  /// If a [Contact] with the same id already exists, it will be replaced.
+  Future<void> saveContactModel(ContactModel contact) =>
+      _contactsApi.saveContact(contact);
+
+  /// Deletes the `ContactModel` with the given id.
+  ///
+  /// If no `ContactModel` with the given id exists, a [ContactModelNotFoundException] error is
+  /// thrown.
+  Future<void> deleteContactModel(String id) => _contactsApi.deleteContact(id);
+
+  /// Sets the `isFavorite` state of single ContactModels to the given value.
+  ///
+  Future<void> favoriteContact({required String id}) =>
+      _contactsApi.favorite(id: id);
+}
